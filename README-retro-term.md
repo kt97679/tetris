b8be899672899eea61511a23d3de14fe30f661e5
rojenzaman <rojen@riseup.net>
2020-10-18 14:09:52 +0300

    add retro support, now only bash support

---
+++ README-retro-term.md
@@ -0,0 +1,34 @@
+Tetris for Retro Term
+===========
+
+Tetris 1984 simulation with `cool-retro-term`.
+
+
+### Original Tetris:
+
+<img src="original-tetris-dvk-screenshot.png" width="100%" alt="Original Tetris">
+
+### Tetris 1984 With Cool-Retro-Term: 
+
+<img src="media/retro-term.png" width="100%" alt="Cool-Retro-Term">
+
+## Usage
+```text
+./run-in-retro-term.sh -h
+usage : ./run-in-retro-term.sh [-h] [-v]
+-h   help
+-v   verbose
+```
+Run `tetris` for executing in Retro Term with background playing [music](media/tetris-theme.mp3).
+
+
+```bash
+$ ./run-in-retro-term.sh  # will be start 
+```
+
+> This music file is licensed under the Creative Commons Attribution-Share Alike 3.0 Unported license. I get from [this source](https://commons.wikimedia.org/wiki/File:Tetris_theme.ogg) and I did not change the original file.
+
+
+## Playing Video
+
+<a href="https://www.youtube.com/watch?v=dO2xVWQj59k"><img src="https://img.youtube.com/vi/dO2xVWQj59k/0.jpg" width="100%" alt="video"></a>
---
+++ run-in-retro-term.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+#
+# cool-retro-term support for tetris.sh
+# Author: Rojen Zaman <rojen@riseup.net>
+# License: WTFPL
+
+set -u                                                    # return error if string null
+stdout="/dev/null"                                        # default stdout, optional verbose mode is /dev/stdout
+script_dir=$(cd $(dirname $0) && pwd)                     # script dir, it is require for cool-retro-term
+
+# usage
+usage() {
+cat <<- EOT    
+usage : $0 [-h] [-v]
+-h   help
+-v   verbose
+EOT
+exit 0
+}
+
+# check cool-retro-term and xdg-open commands
+check_commands() {
+    if ! command -v xdg-open &>$stdout
+    then
+        echo -e "xdg-open could not be found, please install it.\naborting."
+        echo "if sure you at the gnu operating systems?  :)"
+        exit 1
+    fi
+
+    if ! command -v cool-retro-term &>$stdout
+    then
+        echo -e "cool-retro-term could not be found, please install it.\naborting."
+        exit 1
+    fi
+}
+
+# main script
+main() {
+    run_music() {
+        while :
+        do                                                      # play music with xdg-open
+            xdg-open "$script_dir/media/tetris-theme.mp3" &>$stdout
+            sleep 85                                           # sleep 85, 85 is total of music time.
+        done
+    }
+    echo "tetris theme is loading.."                            # music message
+    run_music &                                                 # run music loop at background
+    sleep 1;                                                    # sleep 1 for script run after playing the music.
+    cool-retro-term --fullscreen --profile "Monochrome Green" -e "$script_dir/tetris.sh" &>$stdout
+    echo "exit from cool-retro-term"
+
+    kill $! &>$stdout                                           # kill music loop
+    fuser -k -TERM $script_dir/media/tetris-theme.mp3 &>$stdout # fuse music after program stopped
+    echo "music stopped.."
+}
+
+while getopts ":hv" opt; do                                     # check script
+  case ${opt} in
+    h )
+        usage
+        exit 0
+      ;;  
+    v )
+        stdout="/dev/stdout"                                    # verbose mode
+      ;;
+  esac
+done 
+
+check_commands                                                  # check commands before run script
+main                                                            # program start here
