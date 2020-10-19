7891b39316e98595755aa3f50b5509537c701782
rojenzaman <rojen@riseup.net>
2020-10-19 12:17:16 +0300

    fix some bugs, code beatufiy, add console mode select support

--- run-in-retro-term.sh
+++ run-in-retro-term.sh
@@ -8,29 +8,56 @@ stdout="/dev/null"                                                              
 script_dir=$(dirname $(readlink -f $0))                                                 # script dir, it is required for cool-retro-term
 pl=${VARIABLE:-bash}                                                                    # if pl not specified return bash
 hl=${VARIABLE:-english}                                                                 # default language for bash
+console_profile="Monochrome Green"                                                      # default console profile
 available_languages=$(ls -I README.md $script_dir/lang/ 2>/dev/null | cut -d. -f1)      # https://github.com/rojen/tetris/blob/master/lang/README.md
+
+usage_message_1() {
+printf "%s\n" \
+    "" \
+    "usage : $0 [-h] [-v] [-s PL] [-c console]" \
+    "" \
+    " -h     help" \
+    " -v     verbose" \
+    "" \
+    " -s     select programming language of tetris" \
+    "    c" \
+    "    java" \
+    "    javascript" \
+    "    perl" \
+    "    python" \
+    "    ruby" \
+    "" \
+    "    bash [-l language]  select human language of tetris (only work with bash):"
+}      
+
+usage_message_2() {
+printf "%s\n" \
+    "" \
+    "english" \
+    "$available_languages" \
+    ""
+}
+
+usage_message_3() {
+printf "%s\n" \
+    "" \
+    " -c    console profile" \
+    "    1      Default Amber" \
+    "    2      Monochrome Green" \
+    "    3      Green Scanlines" \
+    "    4      Default Pixelated" \
+    "    5      Apple ][" \
+    "    6      Vintage" \
+    "    7      IBM Dos" \
+    "    8      IBM 3278" \
+    "    9      Futuristic"
+}
 
 # usage
 usage() {
-    printf "%s\n" \
-        "" \
-        "usage : $0 [-h] [-v] [-s PL]" \
-        "" \
-        " -h     help" \
-        " -v     verbose" \
-        "" \
-        " -s     select programming language of tetris" \
-        "    c" \
-        "    java" \
-        "    javascript" \
-        "    perl" \
-        "    python" \
-        "    ruby" \
-        "" \
-        "    bash [-l language]  select human language of tetris (only work with bash):" \
-        "" \
-        "english" \
-        "$available_languages"
+    usage_message_1
+    usage_message_2
+    usage_message_3
     exit 0
 }
 
@@ -49,6 +76,7 @@ check_commands() {
 check_hl() {
     [ "$pl" != "bash" ] && {                                                            # default language is english, see 10th line
         echo "only bash support internationalization, aborting."
+        usage_message_2
         exit 1
     }
 }
@@ -56,8 +84,17 @@ check_hl() {
 check_pl() {
     [[ "$pl" =~ ^(bash|c|java|javascript|perl|python|ruby)$ ]] || {                     # check pl       
         echo "$pl is not supported yet, aborting."
-        check_hl
+        usage_message_1
+        usage_message_3
         exit 1                                                                          # stop if wrong pl given
+    }
+}
+
+check_console_profile() {
+    [[ $console_profile =~ ^(1|2|3|4|5|6|7|8|9)$ ]] || {                                # check console profile
+        echo "console profile $console_profile not finded."
+        usage_message_3
+        exit 1
     }
 }
 
@@ -69,64 +106,59 @@ run_music() {
     done
 }
 
-# scripts for execute tetris.(*)                                                        # cool-retro-term does not support multiple commands or arguments
+# functions for execute tetris.(*)                                                      # set exec_tetris for user specification
 load_exec() {
-    exec_bash() {
-        echo "$script_dir/tetris.sh -l $hl" >>$script_dir/retro_term_command.sh
-    }
-    exec_c() {
-        echo "gcc $script_dir/tetris.c -o /tmp/tetris && /tmp/tetris" >>$script_dir/retro_term_command.sh
-    }
-    exec_java() {
-        echo "javac -d /tmp/ $script_dir/Tetris.java && java -classpath /tmp Tetris" >>$script_dir/retro_term_command.sh
-    }
-    exec_javascript() {
-        echo "node $script_dir/tetris.js" >>$script_dir/retro_term_command.sh
-    }
-    exec_perl() {
-        echo "perl $script_dir/tetris.pl" >>$script_dir/retro_term_command.sh
-    }
-    exec_python() {
-        echo "python $script_dir/tetris.py" >>$script_dir/retro_term_command.sh
-    }
-    exec_ruby() {
-        echo "ruby $script_dir/tetris.rb " >>$script_dir/retro_term_command.sh
-    }
+    [ "$pl" == "bash" ] && exec_tetris="$script_dir/tetris.sh -l $hl"
+    [ "$pl" == "c" ] && exec_tetris="gcc $script_dir/tetris.c -o /tmp/tetris && /tmp/tetris"
+    [ "$pl" == "java" ] && exec_tetris="javac -d /tmp/ $script_dir/Tetris.java && java -classpath /tmp Tetris"
+    [ "$pl" == "javascript" ] && exec_tetris="node $script_dir/tetris.js"
+    [ "$pl" == "perl" ] &&  exec_tetris="perl $script_dir/tetris.pl"
+    [ "$pl" == "python" ] &&  exec_tetris="python $script_dir/tetris.py"
+    [ "$pl" == "ruby" ] &&  exec_tetris="ruby $script_dir/tetris.rb "
+}
+
+# load console profile, defaul is Monochrome Green
+load_console_profile() {
+    [ "$console_profile" = "1" ] && console_profile="Default Amber"
+    [ "$console_profile" = "2" ] && console_profile="Monochrome Green"
+    [ "$console_profile" = "3" ] && console_profile="Green Scanlines"
+    [ "$console_profile" = "4" ] && console_profile="Default Pixelated"
+    [ "$console_profile" = "5" ] && console_profile="Apple ]["
+    [ "$console_profile" = "6" ] && console_profile="Vintage"
+    [ "$console_profile" = "7" ] && console_profile="IBM Dos"
+    [ "$console_profile" = "8" ] && console_profile="IBM 3278"
+    [ "$console_profile" = "9" ] && console_profile="Futuristic"
 }
 
 # main script
 main() {
-    echo "#!/bin/bash" > $script_dir/retro_term_command.sh                              # because cool-retro-term should be able to see that the file is a bash script
-    load_exec && chmod 775 $script_dir/retro_term_command.sh                            # load PL's functions, given 5 to other users because for the cool-retro-term can execute it
-    exec_$pl                                                                            # execute the specified pl
-    echo "read -rsn1 -p 'Press any key to exit'" >> $script_dir/retro_term_command.sh   # press any key to exit  / if there is a execute error it shows the reason
     echo "tetris theme is loading.."                                                    # music message
     run_music &                                                                         # run music loop at background
     sleep 1                                                                             # sleep 1 for script run after playing the music.
     cool-retro-term \
     --fullscreen \
-    --profile "Monochrome Green" \
-    -e "$script_dir/retro_term_command.sh" &>$stdout                                    # cool-retro-term deos not support multiple arguments or commands
-    echo "exit from cool-retro-term"                                                    # ext retro message
+    --profile "$console_profile" \
+    -e bash -c \
+    "$exec_tetris;read -rsn1 -p 'Press any key to exit'" &>$stdout                      # execute specified pl - press any key to exit  / if there is a execute error it shows the reason - default console profile
+    echo "exit from cool-retro-term"                                                    # exit retro message
     kill $! &>$stdout                                                                   # kill music loop
     fuser -k -TERM $script_dir/media/tetris-theme.ogg &>$stdout                         # fuse music after program stopped
     echo "music stopped.."
 }
 
-while getopts ":hvs:l:" opt; do                                                         # check script
+while getopts ":hvs:l:c:" opt; do                                                         # check script
   case ${opt} in
     h ) usage ;;
     v ) stdout="/dev/tty" ;;                                                            # verbose mode
-    s ) pl=${OPTARG} ;;
-    l ) hl=${OPTARG} ;;
+    s ) pl=${OPTARG}; check_pl ;;                                                       # set and check pl specification
+    l ) hl=${OPTARG}; check_hl ;;                                                       # set and check hl specification
+    c ) console_profile=${OPTARG}; check_console_profile ;;                             # set and check console profile
     : ) echo -e "Missing option argument for -$OPTARG\n"; usage; exit 1 ;;
   esac
 done
 
-
 check_commands                                                                          # check commands before run script
 set -u                                                                                  # return error if string null
-check_pl                                                                                # check pl specifion
-check_hl                                                                                # check hl specifion
+load_console_profile                                                                    # load console profile
+load_exec                                                                               # load PL's functions
 main                                                                                    # program start here
-rm $script_dir/retro_term_command.sh &>$stdout                                          # delete trash file
